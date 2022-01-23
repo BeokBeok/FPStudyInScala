04ec551c44e46138e0c6931f309f560fb5ce0127
강현석 <kekemusa37@gmail.com>
2022-01-24 00:05:18 +0900

    [추가] factorial 함수

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -1,6 +1,18 @@
+import scala.annotation.tailrec
+
 object MyModule {
   def abs(n: Int): Int =
     if (n < 0) -n else n
+
+  def factorial(n: Int): Int = {
+    @tailrec
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+    }
+
+    go(n, 1)
+  }
 
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
