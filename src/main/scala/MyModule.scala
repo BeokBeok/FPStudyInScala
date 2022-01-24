6be8c895cbca5221031ab0dbb8f7d544c88b6a2c
강현석 <kekemusa37@gmail.com>
2022-01-24 23:01:26 +0900

    [구현] isSorted 함수

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -39,6 +39,17 @@ object MyModule {
     msg.format(name, n, f(n))
   }
 
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @tailrec
+    def go(n: Int): Boolean = {
+      if (n >= as.length) true
+      else if (ordered(as(n), as(n + 1))) false
+      else go(n + 1)
+    }
+
+    go(0)
+  }
+
   def main(args: Array[String]): Unit =
     println(formatResult("The absolute value", -42, abs))
     println(formatResult("factorial", 7, factorial))
