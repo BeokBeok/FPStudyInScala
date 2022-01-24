80fcc331baed35d9cec6d570d54e7c4340dfd939
강현석 <kekemusa37@gmail.com>
2022-01-24 23:34:29 +0900

    [구현] uncurry 함수

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -54,6 +54,10 @@ object MyModule {
     a => b => f(a,b)
   }
 
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a, b) => f(a)(b)
+  }
+
   def main(args: Array[String]): Unit =
     println(formatResult("The absolute value", -42, abs))
     println(formatResult("factorial", 7, factorial))
