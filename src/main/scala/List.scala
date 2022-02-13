713d0a0ce31e3d9af410e71d7bb744df41522b83
강현석 <kekemusa37@gmail.com>
2022-02-13 21:50:34 +0900

    [문제풀이] 연습문제 3.4

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -27,6 +29,14 @@ object List {
     case Nil => sys.error("setHead on empty list")
     case Cons(_, t) => Cons(h, t)
   }
+
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if n <= 0 then l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n-1)
+    }
 
   def main(args: Array[String]): Unit =
     val x = List(1, 2, 3, 4, 5) match {
